2c3ca6891356ba3b98a92907a13f250974f0a5de
barbosajuh <julia.magalhaes@estudante.ifms.edu.br>
2026-06-07 17:05:11 -0400

    oitavo comit

---
+++ ex8.js
@@ -0,0 +1,36 @@
+class Funcionario {
+    constructor(nome, salario, cargo) {
+        this.nome = nome;
+        this.salario = salario;
+        this.cargo = cargo;
+    }
+
+    calcularSalarioTotal() {
+        return this.salario;
+    }
+}
+
+class Gerente extends Funcionario {
+    constructor(nome, salario, cargo, bonus) {
+        super(nome, salario, cargo);
+        this.bonus = bonus;
+        this.equipe = [];
+    }
+
+    adicionarMembro(funcionario) {
+        this.equipe.push(funcionario);
+    }
+
+    calcularSalarioTotal() {
+        return this.salario + this.bonus;
+    }
+}
+
+
+const gerente = new Gerente("Julia", 5000, "Gerente", 1000);
+
+gerente.adicionarMembro("Carlos");
+gerente.adicionarMembro("Ana");
+
+console.log(gerente.equipe);
+console.log(gerente.calcularSalarioTotal());
